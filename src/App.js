b15876968f3b96e66637d68ab8a02d862ee5f717
Eloi Gallés <egalles@cetrex.net>
2021-11-25 17:03:58 +0100

    add first initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
@@ -18,7 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+      <Footer />
+    </div>    
   );
 }
 
